9d28cdf3f278430b7cae7fba72fe3a93f075aac6
hsonthach <son.thach011011@gmail.com>
2024-10-15 13:52:46 -0500

    lab10 implement key change

--- lab10/my-app/src/App.tsx
+++ lab10/my-app/src/App.tsx
@@ -1,8 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 import axios from "axios";
 
 interface User {
+    id: string;
     name: string;
     avatar_url: string;
     profileUrl: string;
@@ -24,34 +25,38 @@ function UserCard({avatar, name, profileUrl}: UserCardProps) {
 }
 
 function App() {
-    const [key, setKey] = useState("hello");
+    const [key, setKey] = useState("");
     const [users, setUsers] = useState<User[]>([]);
+    const inputRef = useRef<HTMLInputElement>(null);
     useEffect(() => {
+        if (!key) return;
         axios.get(`https://api.github.com/search/users?q=${key}`).then(res => {
             console.log(res.data);
             const users = res.data.items.map((item: any) => {
                 return {
                     name: item.login,
                     avatar_url: item.avatar_url,
-                    profileUrl: item.html_url
+                    profileUrl: item.html_url,
+                    id: item.id
                 }
             })
             setUsers(users);
         })
-    }, []);
-    console.log(users)
+    }, [key]);
     return (
         <div className="App">
             <div className="container">
                 <section className="jumbotron">
                     <h3 className="jumbotron-heading">Search Github Users</h3>
                     <div>
-                        <input type="text" placeholder="enter the name you search"/>&nbsp;
-                        <button>Search</button>
+                        <input type="text" placeholder="enter the name you search" ref={inputRef} />&nbsp;
+                        <button onClick={()=>{
+                            setKey(inputRef.current?.value || "");
+                        }}>Search</button>
                     </div>
                 </section>
                 <div className="row">
-                    {users.map(user => <UserCard name={user.name} avatar={user.avatar_url} profileUrl={user.profileUrl}/>)}
+                    {users.map(user => <UserCard key={user.id} name={user.name} avatar={user.avatar_url} profileUrl={user.profileUrl}/>)}
                 </div>
             </div>
         </div>
