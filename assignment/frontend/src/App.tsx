191daafe2989b59473c294aa5a99c0458bf6c21c
hsonthach <son.thach011011@gmail.com>
2024-10-22 10:03:38 -0500

    bootstrap todo app

--- assignment/frontend/src/App.tsx
+++ assignment/frontend/src/App.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from './app/store';
+import { addTodo, toggleTodo, removeTodo } from './features/todo/todoSlice';
 import './App.css';
 
-export default function App() {
+const App: React.FC = () => {
+  const [text, setText] = useState('');
+  const todos = useSelector((state: RootState) => state.todo.todos);
+  const dispatch = useDispatch();
+
+  const handleAddTodo = () => {
+    if (text.trim()) {
+      dispatch(addTodo(text));
+      setText('');
+    }
+  };
+
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-  )
-}
+    <div className="App">
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+              onClick={() => dispatch(toggleTodo(todo.id))}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => dispatch(removeTodo(todo.id))}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
