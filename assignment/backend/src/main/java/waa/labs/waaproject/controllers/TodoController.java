ba2a05ee45a99b31645f8a84250f31a1d8b5d04b
hsonthach <son.thach011011@gmail.com>
2024-10-22 20:49:49 -0500

    Add patch todo

--- assignment/backend/src/main/java/waa/labs/waaproject/controllers/TodoController.java
+++ assignment/backend/src/main/java/waa/labs/waaproject/controllers/TodoController.java
@@ -1,8 +1,7 @@
 package waa.labs.waaproject.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import waa.labs.waaproject.models.Todo;
 import waa.labs.waaproject.services.ITodoService;
 
