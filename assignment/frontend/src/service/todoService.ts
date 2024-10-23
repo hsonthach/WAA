d2fb515fdf8a2a190ad17a52c482caf71d0789e4
aha618054 <aha@miu.edu>
2024-10-23 17:02:32 -0500

    fix create function

--- assignment/frontend/src/service/todoService.ts
+++ assignment/frontend/src/service/todoService.ts
@@ -2,15 +2,26 @@
 import { Todo } from "../features/todo/todoSlice";
 
 const baseUrl = "http://localhost:8080";
+const getHeaders = () => {
+  const token = localStorage.getItem('jwtToken');
+  console.log("Token used in headers:", token);
+  return {
+    'Content-Type': 'application/json',
+    ...(token ? { Authorization: `Bearer ${token}` } : {}),
+  };
+};
+
+
 
 export const createTodoApi = async (text: string): Promise<Todo> => {
   const response = await fetch(`${baseUrl}/todos`, {
     method: "POST",
-    body: JSON.stringify({ title: text }),
+    headers: getHeaders(),
+    body: JSON.stringify({ name: text }),
   });
   if (!response.ok) {
     throw new Error("Failed to create todo");
   }
   const data = await response.json();
-  return { id: data.id, name: data.title, completed: data.completed, description: "" };
+  return { id: data.id, name: data.name, completed: data.completed, description: "" };
 };
