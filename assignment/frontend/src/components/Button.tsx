7d57a45a9d350deea1f2baf9327c06eebe1fab64
Edward <edwardduong71311@gmail.com>
2024-10-23 22:53:24 -0500

    feat(CS545-Tien-3): add spread pops

--- assignment/frontend/src/components/Button.tsx
+++ assignment/frontend/src/components/Button.tsx
@@ -1,14 +1,22 @@
 type Props = {
   text?: string;
-  className?: string;
+  background?: string;
 } & {
   [key: string]: unknown;
+};
+
+export const COLORS = {
+  DEFAULT: "bg-yellow-600 hover:bg-yellow-500",
+  RED: "bg-red-600 hover:bg-red-400",
 };
 
 export default function Button(props: Props) {
   return (
     <button
-      className={`transition-all duration-150 w-full rounded-md bg-yellow-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600 ${props.className}`}
+      {...props}
+      className={`transition-all duration-150 w-full rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600 ${
+        props.background || COLORS.DEFAULT
+      }`}
     >
       {props.text || "Button"}
     </button>
